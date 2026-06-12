4fd5bb07da19d0a4d5e6a1a6b26b03beccd334c3
kashish gupta <161751892+kashishgupta748@users.noreply.github.com>
2026-06-12 12:33:03 +0530

    Initial Commit

---
+++ CreateO/CreateOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateOApp.swift
+//  CreateO
+//
+//  Created by GU on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CreateOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
