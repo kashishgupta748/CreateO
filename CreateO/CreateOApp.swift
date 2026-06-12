c414be3268d679a6eed2b460eb1d450a1edec4ee
kashish gupta <161751892+kashishgupta748@users.noreply.github.com>
2026-06-12 13:06:07 +0530

    fixed the error

--- CreateO/CreateOApp.swift
+++ CreateO/CreateOApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct CreateOApp: App {
+    @State private var authManager = AuthManager()
+    @State private var dataStore = DataStore()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
+                .environment(authManager)
+                .environment(dataStore)
+                .task {
+                    await authManager.restoreSession()
+                    authManager.startAuthStateListener()
+                    await dataStore.bootstrap(authManager: authManager)
+                }
         }
     }
 }
